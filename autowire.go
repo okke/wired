9eb2bcf052d783e6693e9e20d36fc68425158c73
Okke <oftewel@gmail.com>
2018-03-12 23:50:53 +0100

    small code beautification

--- autowire.go
+++ autowire.go
@@ -20,14 +20,11 @@ func init() {
 
 func (autowire *autowire) GetValueFor(wire Scope, obj reflect.Value, field reflect.Value, fieldType reflect.StructField) (reflect.Value, bool) {
 
-	originalValue := internal.GetFieldValueByReflection(obj, field, fieldType)
-
-	if originalValue != nil {
+	if originalValue := internal.GetFieldValueByReflection(obj, field, fieldType); originalValue != nil {
 		return internal.NilValue, false
 	}
 
-	value := wire.ConstructByType(fieldType.Type)
-	if value == nil {
+	if value := wire.ConstructByType(fieldType.Type); value == nil {
 		return reflect.ValueOf(nil), false
 	}
 
