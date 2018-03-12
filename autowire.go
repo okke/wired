177785df135f721fcfc2aa18f8194c2d305f4d27
Okke <oftewel@gmail.com>
2018-03-12 23:50:53 +0100

    small code beautification

--- autowire.go
+++ autowire.go
@@ -18,18 +18,23 @@ func init() {
 	RegisterStructDecorationTag(reflect.TypeOf((*AutoWire)(nil)).Elem(), &autowire{})
 }
 
+// GetValueFor implements the StructDecorationTag interface
+//
 func (autowire *autowire) GetValueFor(wire Scope, obj reflect.Value, field reflect.Value, fieldType reflect.StructField) (reflect.Value, bool) {
 
-	originalValue := internal.GetFieldValueByReflection(obj, field, fieldType)
-
-	if originalValue != nil {
+	// when a field has a value, auto-wiring is not applicable
+	//
+	if originalValue := internal.GetFieldValueByReflection(obj, field, fieldType); originalValue != nil {
 		return internal.NilValue, false
 	}
 
-	value := wire.ConstructByType(fieldType.Type)
-	if value == nil {
+	// when wired does not know how to construct a type, let go
+	//
+	if value := wire.ConstructByType(fieldType.Type); value == nil {
 		return reflect.ValueOf(nil), false
 	}
 
+	// return value that can be auto wired
+	//
 	return reflect.ValueOf(wire.ConstructByType(fieldType.Type)), true
 }
